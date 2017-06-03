2cc7ef4ff4ab296d7f289baeefd7137ebb2786fc
Christoph Benjamin Weber <kriswep@wetainment.com>
2017-06-03 22:44:55 +0200

    remove async func

--- src/seed.js
+++ src/seed.js
@@ -6,7 +6,7 @@ import { times } from 'lodash';
 
 import { db, Author } from './connectors';
 
-(async function seed() {
+(function seed() {
   // create mock data with a seed, so we always get the same
   casual.seed(123);
   db.sync({ force: true }).then(() => {
@@ -25,6 +25,4 @@ import { db, Author } from './connectors';
       }),
     );
   });
-}()).catch((ex) => {
-  console.log(`ERROR: ${ex}`); // eslint-disable-line
-});
+}());

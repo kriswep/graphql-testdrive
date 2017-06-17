759f01aeb951226b192012bae29ec3ecedfb66f0
Christoph Benjamin Weber <kriswep@wetainment.com>
2017-06-17 13:44:35 +0200

    add route tests

--- client/src/routes.js
+++ client/src/routes.js
@@ -6,30 +6,33 @@ import Callback from './Callback';
 import Auth from './Auth';
 import history from './history';
 
-const auth = new Auth();
-
-export const handleAuthentication = (nextState) => {
+export const handleAuthentication = (auth, nextState) => {
+  // const auth = new Auth();
   if (/access_token|id_token|error/.test(nextState.location.hash)) {
     auth.handleAuthentication();
   }
 };
 
-const makeMainRoutes = () =>
-  (<BrowserRouter history={history} component={App}>
-    <div>
-      <Route
-        /* exact*/ path="/"
-        render={props => <App auth={auth} {...props} />}
-      />
-      <Route path="/home" render={props => <Home auth={auth} {...props} />} />
-      <Route
-        path="/callback"
-        render={(props) => {
-          handleAuthentication(props);
-          return <Callback {...props} />;
-        }}
-      />
-    </div>
-  </BrowserRouter>);
+const makeMainRoutes = () => {
+  const auth = new Auth();
+  return (
+    <BrowserRouter history={history} component={App}>
+      <div>
+        <Route
+          /* exact*/ path="/"
+          render={props => <App auth={auth} {...props} />}
+        />
+        <Route path="/home" render={props => <Home auth={auth} {...props} />} />
+        <Route
+          path="/callback"
+          render={(props) => {
+            handleAuthentication(auth, props);
+            return <Callback {...props} />;
+          }}
+        />
+      </div>
+    </BrowserRouter>
+  );
+};
 
 export default makeMainRoutes;
--- client/src/routes.test.js
+++ client/src/routes.test.js
@@ -1,12 +1,11 @@
 /* globals test expect jest */
+import { shallow } from 'enzyme';
+import { Route } from 'react-router';
 import makeMainRoutes, { handleAuthentication } from './routes';
-
-// let Auth = require('./Auth');
-// Auth = jest.fn();
-// const mMock = jest.fn();
-// Auth.mockImplementation(() => ({
-//   handleAuthentication: mMock,
-// }));
+import * as Auth from './Auth';
+import App from './App';
+import Home from './Home';
+import Callback from './Callback';
 
 test('routes should define mainRoute', () => {
   const routes = makeMainRoutes();
@@ -16,12 +15,41 @@ test('routes should define mainRoute', () => {
 test('routes should handle auth', () => {
   // without auth
   expect(
-    handleAuthentication.bind(null, { location: { hash: '' } }),
+    handleAuthentication.bind(null, {}, { location: { hash: '' } }),
   ).not.toThrow();
 
   // with auth
+  const mMock = jest.fn();
   expect(
-    handleAuthentication.bind(null, { location: { hash: 'access_token' } }),
+    handleAuthentication.bind(
+      null,
+      { handleAuthentication: mMock },
+      { location: { hash: 'access_token' } },
+    ),
   ).not.toThrow();
-  // expect(mMock).toHaveBeenCalled();
+  expect(mMock).toHaveBeenCalled();
+});
+
+test('renders correct routes', () => {
+  Auth.default = jest.fn();
+
+  const routes = makeMainRoutes();
+
+  const wrapper = shallow(routes);
+  const routesRender = wrapper.find(Route).reduce((prev, route) => {
+    const routeProps = route.props();
+    // eslint-disable-next-line no-param-reassign
+    prev[routeProps.path] = routeProps.render;
+    return prev;
+  }, {});
+  // { 'nurse/authorization' : NurseAuthorization, ... }
+  expect(typeof routesRender['/']).toBe('function');
+  expect(typeof routesRender['/home']).toBe('function');
+  expect(typeof routesRender['/callback']).toBe('function');
+
+  expect(routesRender['/']().type).toEqual(App);
+  expect(routesRender['/home']().type).toEqual(Home);
+  expect(routesRender['/callback']({ location: { hash: '' } }).type).toEqual(
+    Callback,
+  );
 });
